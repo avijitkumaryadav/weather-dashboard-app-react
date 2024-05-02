f528d5aaa739c9f6aa4aa34a909e7249479f35b8
avijitkumaryadav <kumaravijit010@gmail.com>
2024-05-03 05:25:06 +0530

    weatherapp0.1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://github.com/avijitkumaryadav/weather-dashboard-app-react.git">
+          Download Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="https://www.linkedin.com/in/avijitkumaryadav">
+          Gaurav Ghai
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://developer.mozilla.org/en-US/">
+          HTML HINTS
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
