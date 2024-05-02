94d354ebcdeba68adc6b15a67affa84b19ef5b88
avijitkumaryadav <kumaravijit010@gmail.com>
2024-05-02 23:40:46 +0530

    weather-app-0.1

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a6ce15afa02e45cae997d7c55ef88c26",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
